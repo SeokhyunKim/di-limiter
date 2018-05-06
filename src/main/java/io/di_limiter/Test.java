c875c1c5fe4a3e2439fecbd61242bbaf0020b108
seokhyun <seokhyun.kim.78@gmail.com>
2018-05-06 10:17:57 -0700

    first commit

---
+++ src/main/java/io/di_limiter/Test.java
@@ -0,0 +1,8 @@
+package io.di_limiter;
+
+public class Test {
+
+    public void testMan() {
+        System.out.println("super test");
+    }
+}
---
+++ src/test/java/io/di_limiter/TestTest.java
@@ -0,0 +1,11 @@
+package io.di_limiter;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+    @Test
+    public void firstJUnit5Test() {
+        System.out.println("hi guys");
+    }
+}
